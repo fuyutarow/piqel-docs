9af41e5fd0226787fcc8bae41b2040123e1e66bf
fuyutarow <fuyutarow@gmail.com>
2022-08-22 17:41:39 +0900

    Add pages/*

---
+++ src/pages/cli.tsx
@@ -0,0 +1,41 @@
+import { Component, createSignal, For } from 'solid-js';
+import { nanoid } from 'nanoid'
+import { queryExamples, QueryResource, toSome } from '../utils/queryExamples';
+import { QueryCard } from '../components/QueryCard';
+
+
+
+export default function Home() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <section class="text-gray-700 p-8">
+      <For each={queryExamples}>
+        {resource => (
+          <QueryCard resource={resource} lang="CLI" />
+        )}
+      </For>
+
+      <h1 class="text-2xl font-bold">Home</h1>
+      <p class="mt-4">This is the home page.</p>
+
+      <div class="flex items-center space-x-2">
+        <button
+          class="border rounded-lg px-2 border-gray-900"
+          onClick={() => setCount(count() - 1)}
+        >
+          -
+        </button>
+
+        <output class="p-10px">Count: {count}</output>
+
+        <button
+          class="border rounded-lg px-2 border-gray-900"
+          onClick={() => setCount(count() + 1)}
+        >
+          +
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,32 @@ import { lazy } from 'solid-js';
 import type { RouteDefinition } from 'solid-app-router';
 
 import Home from './pages/home';
+import CLI from './pages/cli';
+import Rust from './pages/rust';
+import Python from './pages/python';
+import JavaScript from './pages/javascript';
 import AboutData from './pages/about.data';
 
 export const routes: RouteDefinition[] = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/cli',
+    component: CLI,
+  },
+  {
+    path: '/rust',
+    component: Rust,
+  },
+  {
+    path: '/python',
+    component: Python,
+  },
+  {
+    path: '/javascript',
+    component: JavaScript,
   },
   {
     path: '/about',
